212f8b232a0f5514784262ac568a6ede66fcd854
Elizabeth Bartholomew <lizzy.bartholomew@gmail.com>
2023-10-29 14:23:30 -0600

    updated

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 function isChecked() {
     var isChecked = document.getElementById("switch").checked;
 
-    let card1 = document.getElementById("card");
-    let card2 = document.getElementById("card-2");
+    let card1 = document.getElementsByClassName("card");
     console.log(isChecked);
     if (isChecked){
         document.body.style.background = 'hsl(0, 0%, 100%)';
         document.body.style.color = 'black';
         card1.style.backgroundColor = 'hsl(227, 47%, 96%)';
-        card2.style.backgroundColor = 'hsl(227, 47%, 96%)';
+        
     }
 
     if (!isChecked){
         document.body.style.background = 'hsl(230, 17%, 14%)';
         card1.style.backgroundColor = 'hsl(228, 28%, 20%)';
-        card2.style.backgroundColor = 'hsl(228, 28%, 20%)';
+        
     }
 }
